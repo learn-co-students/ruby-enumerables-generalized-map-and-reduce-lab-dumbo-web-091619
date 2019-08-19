ad38155e5de616d27f4b33d1300fc0bb8c359bd1
Stephen Rivera <st.rivera_94@yahoo.com>
2019-08-19 19:36:00 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,14 @@
 # Your Code Here
+#map negative, same, double, squared
+def map(source_array)
+  i = 0
+  while i < source_array.length do 
+    puts source_array[i]
+  end
+  i += 1 
+end
+
+map{|n| n * -1}
+
+#reduce total, all true, any true 
+
