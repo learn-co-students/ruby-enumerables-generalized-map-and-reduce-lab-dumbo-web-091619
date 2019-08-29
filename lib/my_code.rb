2c504bd0ced7139c916a64abab930a2872184e3c
Emirhan Kaplan <emskaplann@gmail.com>
2019-08-29 05:58:20 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+source_array=[1,23,4,5,7,12]
+
+def map(source_array)
+  new=[]
+  i=0 
+  while i<source_array.length
+    new << yield(source_array[i])
+    i+=1
+  end
+  return new
+end
+map(source_array){|n| n*-1}
+
+def reduce(source_array,starting_point=0)
+  i=0
+  while i<source_array.length
+  starting_point = yield(starting_point,source_array[i])
+  i+=1
+  end
+  if starting_point==0 
+    starting_point=true
+  end
+  return starting_point
+end
