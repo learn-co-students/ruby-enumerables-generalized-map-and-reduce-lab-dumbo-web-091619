f08cc85a7ccdd24caa33a550cbe94198cb5ffbe9
Gaida M Escobar <gescobar129@gmail.com>
2019-09-09 17:18:44 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
 # Your Code Here
+def map(array)
+  new_array = []
+  i = 0 
+  while i < array.length 
+    new_array.push(yield(array[i]))
+    i += 1 
+    end
+    new_array 
+end
+
+def reduce(array, sp=nil)
+  if sp
+    sum = sp
+    i = 0 
+  else
+    sum = array[0]
+    i = 1 
+  end
+  while i < array.length
+  sum = yield(sum, array[i])
+  i += 1 
+end
+sum
+end
+
+
+
+#sp = starting point. we make second argument optional by giving it  a default value of nil.that means that by default the starting value will not exist.not until we explicitly give it a value.
+
