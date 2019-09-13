161c17027e04c97d1e61860acd86664b55988226
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-09-13 18:14:24 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+def map(array)
+  new_array = []
+  for i in array do
+    new_array.push(yield(i))
+  end
+  new_array
+end 
+
+def reduce (array, starting_point = nil)
+  if starting_point
+    total = starting_point
+    i = 0
+  else 
+    total = array.first 
+    i = 1
+  end
+  while i < array.length do
+    total = yield(total, array[i])
+    i += 1
+  end
+  if total == nil 
+    return false
+  end 
+  total
+end 
